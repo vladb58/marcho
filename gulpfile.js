5ffd64b084d7f9d5ab9a1e650f4f0f98f5f0701c
vladb58 <belonogoff.vladislaw@yandex.ru>
2020-11-21 11:17:11 +0300

    added product

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
